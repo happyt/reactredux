58a27453f3506f4220fe5757ef6bf3468f8a95a8
Ian Morton <ian.morton@sky.uk>
2016-08-18 07:37:18 +0100

    Courses page

---
+++ src/components/common/Header.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Header = () => {
+    return (
+        <nav>
+            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+              {" | "}
+            <Link to="/courses" activeCLassName="active">Courses</Link>
+           {" | "}
+            <Link to="/about" activeCLassName="active">About</Link>
+        </nav>
+    );
+};
+
+export default Header;
